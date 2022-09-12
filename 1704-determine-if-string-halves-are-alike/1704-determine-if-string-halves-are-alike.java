0d95d9a7648e8dfab6a4f902e1b9f71ff07db7f6
Harsh Rajotiya <109665757+harsh-rajotiya@users.noreply.github.com>
2022-09-12 07:51:01 +0530

    Time: 3 ms (92.99%), Space: 40.6 MB (93.41%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -3,21 +3,23 @@ class Solution {
     public boolean halvesAreAlike(String s) {
         String a = s.substring(0, s.length() / 2);
         String b = s.substring(s.length() / 2, s.length());
-        String vowels = "AEIOUaeiou";
-
-        int count1 = 0; //no. of vowels in substring a
-        int count2 = 0; //no. of vowels in substring b
+        int x = 0;
+        int y = 0;
 
         for (int i = 0; i < a.length(); i++) {
-            //It will check if the character at i is present in the vowels string more than               0 times and increase the count of vowels it that string
-            if (vowels.indexOf(a.charAt(i)) >= 0) {
-                count1++;
-            }
-            if (vowels.indexOf(b.charAt(i)) >= 0) {
-                count2++;
+            char c = a.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                x++;
             }
         }
 
-        return count1 == count2;
+        for (int i = 0; i < b.length(); i++) {
+            char c = b.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+                y++;
+            }
+        }
+
+        return (x == y);
     }
 }
