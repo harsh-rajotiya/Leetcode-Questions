8ef934fc88c6113737ad6e6fd3f008a889fb28ee
Harsh Rajotiya <109665757+harsh-rajotiya@users.noreply.github.com>
2022-09-13 13:43:36 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (97.32%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public String longestCommonPrefix(String[] strs) {
+        //Take the first word as prefix and check if other words prefix are equal to this or not by decreasing it by one every time
+
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String ans = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(ans) != 0) {
+                ans = ans.substring(0, ans.length() - 1);
+            }
+        }
+        return ans;
+    }
+}
