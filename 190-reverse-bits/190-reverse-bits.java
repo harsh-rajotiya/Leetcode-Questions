605ef694744338df6c7a96cc975b73368f798ee9
Harsh Rajotiya <109665757+harsh-rajotiya@users.noreply.github.com>
2022-09-15 15:02:03 +0530

    Time: 2 ms (32.06%), Space: 42.1 MB (74.11%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.java
+++ 190-reverse-bits/190-reverse-bits.java
@@ -2,6 +2,14 @@ public class Solution {
 
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        return Integer.reverse(n);
+        int reverse = 0;
+
+        for (int i = 0; i < 32; i++) {
+            reverse = reverse << 1; //it will create a space at the place of last bit removed
+            reverse = reverse | (n & 1); //it will take the last bit and will save it in reverse variable
+            n = n >> 1; //last digit will be removed
+        }
+
+        return reverse;
     }
 }
