cbf0c1a5eb083871acaf33ceaa530f2e9095d767
Harsh Rajotiya <109665757+harsh-rajotiya@users.noreply.github.com>
2022-09-16 18:53:27 +0530

    Time: 11 ms (79.58%), Space: 40.9 MB (72.45%) - LeetHub

--- 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.java
+++ 762-prime-number-of-set-bits-in-binary-representation/762-prime-number-of-set-bits-in-binary-representation.java
@@ -13,19 +13,16 @@ class Solution {
     }
 
     static boolean isPrime(int n) {
-        if (n <= 1) return false;
-        if (n <= 3) return true;
-
-        int temp = 0;
-        for (int i = 2; i < n - 1; i++) {
-            if (n % i == 0) {
-                temp += 1;
-            }
-        }
-        if (temp > 0) {
+        if (n < 2) {
             return false;
-        } else {
-            return true;
         }
+        int c = 2;
+        while (c * c <= n) {
+            if (n % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return true;
     }
 }
