049d177b5a3895e6f32edd01947294bf1fa6f939
Harsh Rajotiya <109665757+harsh-rajotiya@users.noreply.github.com>
2022-09-18 18:49:38 +0530

    Time: 19 ms (10.92%), Space: 41.2 MB (26.69%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,10 +1,10 @@
 class Solution {
 
     public int fib(int n) {
-        if (n == 0 || n == 1) {
+        if (n < 2) {
             return n;
-        } else {
-            return fib(n - 1) + fib(n - 2);
         }
+
+        return fib(n - 1) + fib(n - 2);
     }
 }
