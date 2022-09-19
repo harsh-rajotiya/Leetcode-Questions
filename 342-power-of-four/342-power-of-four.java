81a8cf498123820b2dc8fa8bb96d8187d6e634b1
Harsh Rajotiya <109665757+harsh-rajotiya@users.noreply.github.com>
2022-09-19 18:49:27 +0530

    Time: 2 ms (64.16%), Space: 41.4 MB (46.81%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,11 +1,9 @@
 class Solution {
 
-    public boolean isPowerOfFour(int n) {
-        if (n == 0) return false;
-        // find `log4(n)`
-        double i = Math.log(n) / Math.log(4);
-
-        // return true if `log4(n)` is an integer
-        return i == Math.floor(i);
+    public boolean isPowerOfFour(int num) {
+        if (num <= 0) return false;
+        if (num % 4 == 0) return isPowerOfFour(num / 4);
+        if (num == 1) return true;
+        return false;
     }
 }
